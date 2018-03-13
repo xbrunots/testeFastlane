acd11bb72816a451f9798d82a634a054d078e636
Bruno Brito <xbrunots@gmail.com>
2018-03-13 09:40:52 -0300

    testes commit slack

--- app/src/main/java/com/xpi/brunot0655/integracao/MainActivity.java
+++ app/src/main/java/com/xpi/brunot0655/integracao/MainActivity.java
@@ -2,9 +2,14 @@ package com.xpi.brunot0655.integracao;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
 import com.microsoft.appcenter.AppCenter;
 import com.microsoft.appcenter.analytics.Analytics;
 import com.microsoft.appcenter.crashes.Crashes;
+import com.microsoft.appcenter.push.Push;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -13,7 +18,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         AppCenter.start(getApplication(), "47b1f8d9-a4e1-42aa-a67a-4d083f379a07",
-                Analytics.class, Crashes.class);
+                Analytics.class, Crashes.class, Push.class);
+
+        Crashes.setEnabled(true);
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Crashes.generateTestCrash();
+            }
+        });
+
     }
 
 
