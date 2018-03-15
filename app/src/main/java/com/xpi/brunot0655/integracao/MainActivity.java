7230e7c72724f318e51a900ffd4681241fb86526
Bruno Brito <xbrunots@gmail.com>
2018-03-15 11:36:54 -0300

    testando appcenter

--- app/src/main/java/com/xpi/brunot0655/integracao/MainActivity.java
+++ app/src/main/java/com/xpi/brunot0655/integracao/MainActivity.java
@@ -5,6 +5,10 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
+import com.crashlytics.android.Crashlytics;
+
+import io.fabric.sdk.android.Fabric;
+
 import com.microsoft.appcenter.AppCenter;
 import com.microsoft.appcenter.analytics.Analytics;
 import com.microsoft.appcenter.crashes.Crashes;
@@ -15,12 +19,15 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_main);
+
 
         AppCenter.start(getApplication(), "47b1f8d9-a4e1-42aa-a67a-4d083f379a07",
                 Analytics.class, Crashes.class, Push.class);
 
         Crashes.setEnabled(true);
+
 
         findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
             @Override
