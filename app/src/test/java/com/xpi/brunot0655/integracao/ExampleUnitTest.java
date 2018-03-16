e978076f55a72f25c3c93c0944297e3b5b51cc54
Bruno Brito <xbrunots@gmail.com>
2018-03-16 10:37:04 -0300

    testando CI

--- app/src/test/java/com/xpi/brunot0655/integracao/ExampleUnitTest.java
+++ app/src/test/java/com/xpi/brunot0655/integracao/ExampleUnitTest.java
@@ -1,5 +1,6 @@
 package com.xpi.brunot0655.integracao;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -10,8 +11,10 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+
+
     @Test
     public void addition_isCorrect() throws Exception {
-        assertEquals(4, 3 + 2);
+        assertEquals(4, 2 + 2);
     }
 }
